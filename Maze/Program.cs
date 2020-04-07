1032f16429d6eb726f69300465cd0a2456c2759d
Dima <Dima@DESKTOP-UNA3KMK>
2020-04-07 14:34:36 +0300

    Новое начало

---
+++ Maze/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MazeStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+            Maze MyMaze = new Maze(21);
+            MyMaze.ShowMaze();
+            MyMaze.ShowGoodLookMaze();
+            Console.ReadKey();
+            } while (true);
+
+        }
+    }
+}
